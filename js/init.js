bf5ca9794cb13edb727e63b39de897e779ddccc3
chatvelarde <82489536+chatvelarde@users.noreply.github.com>
2021-04-13 23:00:33 -0500

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -1,1 +1,18 @@
-
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyB2SD3RwDHuBtR7ytHun2dKtxD5j1m65XQ",
+    authDomain: "kevinproyecto43.firebaseapp.com",
+    projectId: "kevinproyecto43",
+    storageBucket: "kevinproyecto43.appspot.com",
+    messagingSenderId: "230196695011",
+    appId: "1:230196695011:web:678f03d1d121cdf4b85ad6",
+    measurementId: "G-MGDRDPR9JC"
+});
